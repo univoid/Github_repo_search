d6b476c548efac66ea83dce75044c37d0135d3b9
YuHui.Zheng <zyhlxw@gmail.com>
2017-07-30 12:51:57 +0900

    project Initialization

---
+++ Github_repo_search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Github_repo_search
+//
+//  Created by YUHUI ZHENG on 2017/07/30.
+//  Copyright © 2017 YUHUI ZHENG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
